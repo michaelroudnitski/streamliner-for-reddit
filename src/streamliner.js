555785075cad5b2f9c95dd67fbdb3afdd922e7fb
michaelroudnitski <michaelroudnitski@gmail.com>
2018-08-11 19:42:13 -0400

    stream url is parsed from the best comment

--- src/streamliner.js
+++ src/streamliner.js
@@ -11,11 +11,15 @@ function getComments(e) {
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         const bestComment = JSON.parse(xhr.response)[1].data.children[0].data;
-        console.log(bestComment.body);
+        const start = bestComment.body.indexOf('http');
+        // loop through the comment starting from the 'http' substring and find where reddit closes the href using ')'
+        // var end will be accessible to the outer scope with the index of ')'
+        for (var end = start; bestComment.body[end] != ')'; ++end) {}
+        const stream_url = bestComment.body.substring(start, end);
       }
     }
     xhr.open(
-      'GET', 
+      'GET',
       `https://www.reddit.com/r/soccerstreams/comments/${getT3(e.url)}/.json?sort=best`,
       true
     )
