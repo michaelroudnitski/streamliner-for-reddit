78a27de36589fc3c102f5eb7778a9b1ac00184c5
michaelroudnitski <michaelroudnitski@gmail.com>
2018-08-11 19:20:02 -0400

    can now get the body/content of the best comment in a thread

--- src/streamliner.js
+++ src/streamliner.js
@@ -6,14 +6,19 @@ function getT3(url) {
 }
 
 function getComments(e) {
-  if (!_.endsWith(e.url, '.json')) { // this if statement prevents an infinite loop of get requests
+  if (!_.endsWith(e.url, '.json?sort=best')) { // this if statement prevents an infinite loop of get requests
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        console.log(xhr.response);
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const bestComment = JSON.parse(xhr.response)[1].data.children[0].data;
+        console.log(bestComment.body);
       }
     }
-    xhr.open('GET', `https://www.reddit.com/r/soccerstreams/comments/${getT3(e.url)}/.json`, true)
+    xhr.open(
+      'GET', 
+      `https://www.reddit.com/r/soccerstreams/comments/${getT3(e.url)}/.json?sort=best`,
+      true
+    )
     xhr.send();
   }
 }
