870ace2eb899466a072f592bb209191cfe4087a7
michaelroudnitski <michaelroudnitski@gmail.com>
2018-08-11 19:00:31 -0400

    comments can be requested for the post the user just clicked

--- src/streamliner.js
+++ src/streamliner.js
@@ -1,12 +1,24 @@
 import _ from 'lodash';
 // import subreddits from './subreddits';
 
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    console.log(xhr.response);
+function getT3(url) {
+  return _.split(url, '/')[6]
+}
+
+function getComments(e) {
+  if (!_.endsWith(e.url, '.json')) { // this if statement prevents an infinite loop of get requests
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.response);
+      }
+    }
+    xhr.open('GET', `https://www.reddit.com/r/soccerstreams/comments/${getT3(e.url)}/.json`, true)
+    xhr.send();
   }
 }
-console.log("request sent");
-xhr.open('GET', `https://www.reddit.com/r/soccerstreams/best/.json?limit=2`, true)
-xhr.send();
+
+chrome.webRequest.onBeforeRequest.addListener(
+  getComments,
+  { urls: ["*://*.reddit.com/r/soccerstreams/comments/*"] }
+)
--- src/subreddits.js
+++ src/subreddits.js
@@ -1,4 +1,4 @@
-module.exports = {
-  "soccer": "soccerstreams",
-  "hockey": "NHLstreams"
-}
+module.exports = [
+  "soccerstreams",
+  "NHLstreams"
+]
