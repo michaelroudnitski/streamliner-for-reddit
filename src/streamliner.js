aa2d28b7e5d3d1cc870e448ea55b4c554cc4ebfe
michaelroudnitski <michaelroudnitski@gmail.com>
2018-08-11 22:09:45 -0400

    streamliner will now loop comments until it finds a url

--- src/streamliner.js
+++ src/streamliner.js
@@ -16,14 +16,19 @@ function getComments(e) {
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         try {
-          const bestComment = JSON.parse(xhr.response)[1].data.children[0].data;
-          // loop through the comment starting from the 'http' substring and find where reddit closes the href using ')'
-          // var end will be accessible to the outer scope and will be the index of ')'
-          const start = bestComment.body.indexOf('http');
-          for (var end = start; bestComment.body[end] != ')'; ++end) { }
-          const stream_url = bestComment.body.substring(start, end);
-          chrome.tabs.create({ url: stream_url });
-        } catch (err) {}
+          const comments = JSON.parse(xhr.response)[1].data.children;
+          _.forEach(comments, (comment) => {
+            // loop through the comment starting from the 'http' substring and find where reddit closes the href using ')' or ' '
+            // var end will be accessible to the outer scope and will be the index of ')' or ' '
+            const start = comment.data.body.indexOf('http');
+            if (start != -1) {
+              for (var end = start; comment.data.body[end] != ')' && comment.data.body[end] != ' '; ++end) { }
+              const stream_url = comment.data.body.substring(start, end);
+              chrome.tabs.create({ url: stream_url });
+              return false; // break lodash's foreach loop earlys
+            }
+          })
+        } catch (err) { }
       }
     }
     xhr.open(
